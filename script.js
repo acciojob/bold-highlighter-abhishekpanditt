26ed71e06e7b87cd9e38c70f441a0f396dc1a946
abhishekpanditt <123313123+abhishekpanditt@users.noreply.github.com>
2023-07-18 13:19:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 function highlight() {
     //Write your code here
-
-
+	const bold = document.getElementsByTagName('strong');
+		for(let i = 0; i < bold.length; i++){
+		   bold[i].style.color = 'green';
+	}
 }
 
 
 function return_normal() {
     //Write your code here
-
-    
+    const bold = document.getElementsByTagName('strong');
+		 for(let i = 0; i < bold.length; i++){
+		   bold[i].style.color = 'black';
+		 }
 }
